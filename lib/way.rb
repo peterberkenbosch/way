e3b1b30b10afe9cbe5f7dc6a9fad67ef304d13a3
Caleb Cohoon <calebcohoon@gmail.com>
2012-05-02 09:16:53 -0700

    Removing unnecessary class_evals

--- lib/way.rb
+++ lib/way.rb
@@ -1,33 +1,31 @@
 module ActionView
-  if defined? PartialRenderer
-    PartialRenderer.class_eval do
-      def render_partial
-        locals, view, block = @locals, @view, @block
-        object, as = @object, @variable
+  class PartialRenderer
+    def render_partial
+      locals, view, block = @locals, @view, @block
+      object, as = @object, @variable
 
-        if !block && (layout = @options[:layout])
-          layout = find_template(layout)
-        end
-
-        object ||= locals[as]
-        locals[as] = object
-
-        content = @template.render(view, locals) do |*name|
-          view._layout_for(*name, &block)
-        end
-
-        content = layout.render(view, locals){ content } if layout
-
-        if @view.controller && @view.controller.params.has_key?('_way')
-          content << "#{@template.inspect}"
-        end
-
-        content
+      if !block && (layout = @options[:layout])
+        layout = find_template(layout)
       end
+
+      object ||= locals[as]
+      locals[as] = object
+
+      content = @template.render(view, locals) do |*name|
+        view._layout_for(*name, &block)
+      end
+
+      content = layout.render(view, locals){ content } if layout
+
+      if @view.controller && @view.controller.params.has_key?('_way')
+        content << "#{@template.inspect}"
+      end
+
+      content
     end
   end
 
-  Base.class_eval do
+  class Base
     def render_partial options = {}
       local_assigns = options[:locals] || {}
 
--- way.gemspec
+++ way.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'way'
-  s.version = '1.1.1'
+  s.version = '1.1.2'
   s.summary = 'Find partials in your Rails app easily!'
   s.authors = ['Caleb Cohoon']
   s.email = 'calebcohoon@gmail.com'
